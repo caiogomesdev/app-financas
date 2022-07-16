19eded9fde5361eb83891aef8a4df19bcf12018c
Caio Gomes <caiogame100@gmail.com>
2022-07-16 20:42:59 -0300

    chore: move authprovider

--- App.tsx
+++ App.tsx
@@ -3,15 +3,18 @@ import { ThemeProvider } from 'styled-components';
 import { THEME } from './src/theme';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
+import AuthProvider from './src/hooks/auth';
 import Routes from './src/routes';
 
 export default function App() {
   return (
     <ThemeProvider theme={THEME}>
-      <StatusBar style="inverted" />
-      <NavigationContainer>
-        <Routes />
-      </NavigationContainer>
+      <AuthProvider>
+        <StatusBar style="inverted" />
+        <NavigationContainer>
+          <Routes />
+        </NavigationContainer>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
