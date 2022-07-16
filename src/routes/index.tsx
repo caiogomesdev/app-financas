5cbf5ef9f6ecefccf04ea638019bbe2b26c02c13
Caio Gomes <caiogame100@gmail.com>
2022-07-16 20:46:40 -0300

    feat: configure routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import AuthRoutes from './auth.routes';
-// import AppRoutes from './app.routes';
+import AppRoutes from './app.routes';
+import { AuthContext } from '../hooks/auth';
 
 const App: React.FC = () => {
-  return (
-    <AuthRoutes />
-  )
+  const authContext = useContext(AuthContext);
+
+  return authContext?.isLogged ? <AppRoutes /> : <AuthRoutes />
 }
 
 export default App;
