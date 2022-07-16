d99f6129740389780faa3984f837b6c0fc189031
Caio Gomes <caiogame100@gmail.com>
2022-07-16 17:48:08 -0300

    chore: remove status bar

--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,14 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Container, Title, SubTitle, Button, TextButton, Group } from './style'
 import Input from '../../components/input';
 import { useTheme } from 'styled-components/native';
+import { firebase } from '../../services/firebase';
 
 const App: React.FC = () => {
   const THEME = useTheme();
   return(
     <Container>
-      <StatusBar style="auto" />
       <Group>
         <Title>Bem vindo!</Title>
         <SubTitle>Faça o login para continuar</SubTitle>
