8aa08fb58c71021b06bcdca5495e212d404f8704
caiogomesdev <caiogame100@gmail.com>
2022-07-17 15:00:12 -0300

    chore: add flex 1 of section

--- mobile/src/views/Home/styles.ts
+++ mobile/src/views/Home/styles.ts
@@ -2,8 +2,11 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
+  wrap: flex;
   background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
 `
 export const Section = styled.View`
   padding: 0px 20px;
+  width: 100%;
+  flex: 1;
 `
