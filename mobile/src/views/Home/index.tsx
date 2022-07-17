b071afaecaf1a9d6a64489cd2b12fa40521dd290
caiogomesdev <caiogame100@gmail.com>
2022-07-17 11:59:54 -0300

    feat: add tabs component

--- mobile/src/views/Home/index.tsx
+++ mobile/src/views/Home/index.tsx
@@ -1,11 +1,13 @@
-import React from "react";
-import { Container } from "./styles";
-import Header from "../../components/Header";
+import React from 'react';
+import { Container } from './styles';
+import Header from '../../components/Header';
+import Tabs from '../../components/Tabs';
 
 const App: React.FC = () => {
   return (
     <Container>
       <Header></Header>
+      <Tabs />
     </Container>
   );
 };
