7c14505e63d358e3049934f39aabec260bcb4b44
caiogomesdev <caiogame100@gmail.com>
2022-07-17 14:59:39 -0300

    chore: add dateFullToString

--- mobile/src/utils/dateToString.ts
+++ mobile/src/utils/dateToString.ts
@@ -1,5 +1,12 @@
 export const dateToString = (date: Date): string => {
   const month = date.getMonth() + 1;
   const day = date.getDate();
-  return `${day}/${month}`;
+  return `${day.toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}`;
+}
+
+export const dateFullToString = (date: Date): string => {
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const year = date.getFullYear();
+  return `${day.toString().padStart(2, '0')}/${month.toString().padStart(2, '0')}/${year}`;
 }
