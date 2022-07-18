8f68203c95dea5ac5bef022df65adf9dce8f001c
caiogomesdev <caiogame100@gmail.com>
2022-07-17 22:40:52 -0300

    feat: create auth controller and service

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './controllers/auth/auth.controller';
+import { AuthService } from './services/auth/auth.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AuthController],
+  providers: [AppService, AuthService],
 })
 export class AppModule {}
