446175995516098addc1eabc3656a2d530d3d278
caiogomesdev <caiogame100@gmail.com>
2022-07-23 12:11:24 -0300

    chore: add IUserSigninResult

--- back-end/src/modules/user/dtos/user-signin.dto.ts
+++ back-end/src/modules/user/dtos/user-signin.dto.ts
@@ -1,4 +1,11 @@
+import { IUser } from 'src/entities/models';
+
 export interface IUserSigninDto {
   email: string;
   password: string;
+}
+
+export interface IUserSigninResult {
+  user: IUser;
+  token: string;
 }
