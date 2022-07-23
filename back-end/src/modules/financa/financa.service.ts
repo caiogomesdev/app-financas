0290a955c9cfc543bf003cb025cc1cb640158927
caiogomesdev <caiogame100@gmail.com>
2022-07-23 17:56:24 -0300

    feat: add getAllFinancas, createSpendGain and sumAllFinancas on service

--- back-end/src/modules/financa/financa.service.ts
+++ back-end/src/modules/financa/financa.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { ISpendGain, IUser } from 'src/entities/models';
+import { SpendGainRepository } from 'src/repositories/spend-gain/spend-gain.repository';
+import { FinancaCreateDto } from './dtos';
 
 @Injectable()
-export class FinancaService {}
+export class FinancaService {
+  constructor(private readonly spendGainRepository: SpendGainRepository) {}
+
+  getAllFinancas(userId: string): Promise<ISpendGain[]> {
+    return this.spendGainRepository.findAllByUserId(userId);
+  }
+
+  createSpendGain(user: IUser, body: FinancaCreateDto): Promise<ISpendGain> {
+    const spendGain: ISpendGain = {
+      ...body,
+      userId: user,
+    };
+    return this.spendGainRepository.create(spendGain);
+  }
+
+  async sumAllFinancas(userId: string): Promise<number> {
+    const { sum } = await this.spendGainRepository.sumAllPricesByUser(userId);
+    return sum;
+  }
+}
