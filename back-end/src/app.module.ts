5082614ae86b5c9ec804ebe6856af8b1be073509
caiogomesdev <caiogame100@gmail.com>
2022-07-23 12:12:47 -0300

    refactor: remove AuthController

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './modules/auth/auth.controller';
 import { UserService } from './modules/user/user.service';
 import { UserController } from './modules/user/user.controller';
 import { AuthService } from './modules/auth/auth.service';
@@ -8,7 +7,7 @@ import { UserValidator } from './modules/user/user.validator';
 
 @Module({
   imports: [RepositoriesModule],
-  controllers: [AuthController, UserController],
+  controllers: [UserController],
   providers: [UserService, AuthService, UserValidator],
 })
 export class AppModule {}
