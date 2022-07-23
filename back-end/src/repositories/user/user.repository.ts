ac4773717cc74910cd2b46b0634a8428ccc7888f
caiogomesdev <caiogame100@gmail.com>
2022-07-23 10:30:28 -0300

    chore: move folder repositories to src

---
+++ back-end/src/repositories/user/user.repository.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { AppDataSource } from 'src/config/data-source';
+import { IUser } from 'src/entities/models';
+import { User } from 'src/entities/user.entity';
+import { IUserCreate } from './models/user-create.model';
+
+@Injectable()
+export class UserRepository {
+  repository = AppDataSource.getRepository(User);
+
+  async create(body: IUserCreate) {
+    const user = this.repository.create(body);
+    return this.repository.save(user);
+  }
+
+  async findOne(userId: string): Promise<IUser> {
+    return this.repository.findOne({
+      where: { id: userId },
+    });
+  }
+}
