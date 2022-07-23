3a8ead989bfb3472233a323e2201d6aba511c13e
caiogomesdev <caiogame100@gmail.com>
2022-07-23 17:56:43 -0300

    feat: configure routes

--- back-end/src/modules/financa/financa.controller.ts
+++ back-end/src/modules/financa/financa.controller.ts
@@ -1,12 +1,41 @@
-import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { FinancaCreateDto } from './dtos';
+import { FinancaService } from './financa.service';
 
 @Controller('financa')
 export class FinancaController {
-  @Get('')
+  constructor(private readonly financaService: FinancaService) {}
+
+  @Get()
   @UseGuards(AuthGuard('jwt'))
+  @HttpCode(HttpStatus.OK)
   find(@Req() req: Request) {
-    return req.user;
+    return this.financaService.getAllFinancas(req.user.id);
+  }
+
+  @Post('create')
+  @UseGuards(AuthGuard('jwt'))
+  @HttpCode(HttpStatus.CREATED)
+  create(@Req() req: Request, @Body() body: FinancaCreateDto) {
+    return this.financaService.createSpendGain(req.user, body);
+  }
+
+  @Get('total')
+  @UseGuards(AuthGuard('jwt'))
+  @HttpCode(HttpStatus.OK)
+  async count(@Req() req: Request) {
+    const value = await this.financaService.sumAllFinancas(req.user.id);
+    return { value };
   }
 }
