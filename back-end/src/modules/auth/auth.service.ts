78914bb15f34b0c6e13e91d1636070d793570276
caiogomesdev <caiogame100@gmail.com>
2022-07-23 10:34:26 -0300

    feat: create AuthService

--- back-end/src/modules/auth/auth.service.ts
+++ back-end/src/modules/auth/auth.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { sign } from 'jsonwebtoken';
+import { IUser } from 'src/entities/models';
+import { UserRepository } from 'src/repositories/user/user.repository';
+import { JwtPayload } from './models';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  public async createAcessToken(userId: string): Promise<string> {
+    return sign({ userId }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRATION,
+    });
+  }
+
+  public async validateUser({ userId }: JwtPayload): Promise<IUser> {
+    const user = await this.userRepository.findOne(userId);
+    if (!user) {
+      throw new UnauthorizedException('user not found');
+    }
+    return user;
+  }
+
+  private static jwtExtractor(request: Request) {
+    const authHeader = request.headers.authorization;
+    if (!authHeader) {
+      throw new BadRequestException('Bad Request.');
+    }
+    return authHeader.split(' ')[1];
+  }
+
+  public returnJwtExtractor(): (request: Request) => string {
+    return AuthService.jwtExtractor;
+  }
+}
