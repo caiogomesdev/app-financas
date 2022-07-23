921595e2a3dbfa897df30a41a84fbf004d236466
caiogomesdev <caiogame100@gmail.com>
2022-07-23 16:10:31 -0300

    feat: create SpendGainRepository

--- back-end/src/repositories/spend-gain/spend-gain.repository.ts
+++ back-end/src/repositories/spend-gain/spend-gain.repository.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { ISpendGain } from 'src/entities/models';
+import { SpendGain } from 'src/entities/spend-gain.entity';
+import { AppDataSource } from '../../config/data-source';
 
 @Injectable()
-export class SpendGainRepository {}
+export class SpendGainRepository {
+  repository = AppDataSource.getRepository(SpendGain);
+
+  async create(spendGain: ISpendGain): Promise<ISpendGain> {
+    const result = this.repository.create(spendGain);
+    return this.repository.save(result);
+  }
+
+  async findAllById(id: string): Promise<ISpendGain[]> {
+    return this.repository.find({
+      where: {
+        id,
+      },
+    });
+  }
+}
