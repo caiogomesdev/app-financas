e0254cb911782d29ef034ee8f5f33d123e23899a
caiogomesdev <caiogame100@gmail.com>
2022-07-23 13:43:02 -0300

    feat: configure import and providers

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -4,10 +4,28 @@ import { UserController } from './modules/user/user.controller';
 import { AuthService } from './modules/auth/auth.service';
 import { RepositoriesModule } from './repositories/repositories.module';
 import { UserValidator } from './modules/user/user.validator';
+import { FinancaController } from './modules/financa/financa.controller';
+import { FinancaService } from './modules/financa/financa.service';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './modules/auth/strategies/jwt.strategy';
 
 @Module({
-  imports: [RepositoriesModule],
-  controllers: [UserController],
-  providers: [UserService, AuthService, UserValidator],
+  imports: [
+    RepositoriesModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: process.env.JWT_EXPIRATION },
+    }),
+  ],
+  controllers: [UserController, FinancaController],
+  providers: [
+    UserService,
+    AuthService,
+    UserValidator,
+    FinancaService,
+    JwtStrategy,
+  ],
 })
 export class AppModule {}
