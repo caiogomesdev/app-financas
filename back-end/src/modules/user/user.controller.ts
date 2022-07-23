5dc16397c481e9adee7199a2a91682dd95656241
caiogomesdev <caiogame100@gmail.com>
2022-07-23 10:39:17 -0300

    feat: configure UserController

--- back-end/src/modules/user/user.controller.ts
+++ back-end/src/modules/user/user.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { IUserCreateDto } from './dtos';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
   @Post()
-  async create(@Body() body) {
-    await this.userService.create();
+  async create(@Body() body: IUserCreateDto) {
+    await this.userService.create(body);
     return body;
   }
 }
