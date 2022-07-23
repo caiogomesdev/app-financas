07d0f9f085c07d60d931f39f19670094d4c62e02
caiogomesdev <caiogame100@gmail.com>
2022-07-23 16:09:05 -0300

    fix: spend_gain

--- back-end/src/entities/spend-gain.entity.ts
+++ back-end/src/entities/spend-gain.entity.ts
@@ -1,14 +1,24 @@
 import {
+  BeforeInsert,
   Column,
   CreateDateColumn,
   Entity,
+  JoinColumn,
+  ManyToOne,
   PrimaryGeneratedColumn,
 } from 'typeorm';
+import { ISpendGain, IUser } from './models';
+import { User } from './user.entity';
+import { v4 as uuidv4 } from 'uuid';
 
-@Entity('user')
-export class SpendGain {
+@Entity('spend_gain')
+export class SpendGain implements ISpendGain {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  userId: IUser;
 
   @Column()
   title: string;
@@ -19,9 +29,17 @@ export class SpendGain {
   @Column({ type: 'timestamp' })
   date: Date;
 
-  @Column({ nullable: false, type: 'float' })
+  @Column({ type: 'float' })
   price: number;
 
   @CreateDateColumn({ type: 'timestamp' })
   createdAt: Date;
+
+  @BeforeInsert()
+  generatedId() {
+    if (this.id) {
+      return;
+    }
+    this.id = uuidv4();
+  }
 }
