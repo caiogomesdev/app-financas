31d273decba647cba5db5569dc9eae4ef48be796
caiogomesdev <caiogame100@gmail.com>
2022-07-23 10:38:59 -0300

    feat: configure UserService

--- back-end/src/modules/user/user.service.ts
+++ back-end/src/modules/user/user.service.ts
@@ -1,16 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { hash } from 'bcrypt';
-import { UserRepository } from 'src/repositories/user.repository';
+import { UserRepository } from 'src/repositories/user/user.repository';
+import { IUserCreateDto } from './dtos';
 
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
-  async create() {
-    const password = await hash('password', 8);
-    await this.userRepository.create({
-      name: '123',
-      email: '123',
-      password,
-    });
+  async create({ name, email, password }: IUserCreateDto) {
+    const passwordHash = await hash(password, 8);
+    await this.userRepository.create({ name, email, password: passwordHash });
   }
 }
