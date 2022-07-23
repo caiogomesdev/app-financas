bb8be5f00be05e1a0dd9dc635ac8ca6bee820891
caiogomesdev <caiogame100@gmail.com>
2022-07-23 12:13:15 -0300

    feat: add signin on UserService

--- back-end/src/modules/user/user.service.ts
+++ back-end/src/modules/user/user.service.ts
@@ -3,13 +3,15 @@ import { hash } from 'bcrypt';
 import { User } from 'src/entities/user.entity';
 import { UserRepository } from 'src/repositories/user/user.repository';
 import { AuthService } from '../auth/auth.service';
-import { IUserSigninDto, IUserSigniupDto } from './dtos';
+import { IUserSigninDto, IUserSigninResult, IUserSigniupDto } from './dtos';
+import { UserValidator } from './user.validator';
 
 @Injectable()
 export class UserService {
   constructor(
     private readonly userRepository: UserRepository,
     private readonly authService: AuthService,
+    private readonly userValidator: UserValidator,
   ) {}
 
   async signUp({ name, email, password }: IUserSigniupDto): Promise<User> {
@@ -17,5 +19,14 @@ export class UserService {
     return this.userRepository.create({ name, email, password: passwordHash });
   }
 
-  async signin({ email, password }: IUserSigninDto) {}
+  async signin({
+    email,
+    password,
+  }: IUserSigninDto): Promise<IUserSigninResult> {
+    const user = await this.userRepository.findByEmail(email);
+    await this.userValidator.validate(user, password);
+    const token = await this.authService.createAcessToken(user.id);
+    user.password = undefined;
+    return { user, token };
+  }
 }
