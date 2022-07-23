988c5b4dce8b1bb7260307f134026e0edfa197e2
caiogomesdev <caiogame100@gmail.com>
2022-07-23 14:01:31 -0300

    refactor: validate

--- back-end/src/modules/auth/strategies/jwt.strategy.ts
+++ back-end/src/modules/auth/strategies/jwt.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-jwt';
 import { IUser } from 'src/entities/models';
@@ -16,10 +16,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(jwtPayload: JwtPayload): Promise<IUser> {
-    const user = await this.authService.validateUser(jwtPayload);
-    if (!user) {
-      throw new UnauthorizedException();
-    }
-    return user;
+    return this.authService.validateUser(jwtPayload);
   }
 }
