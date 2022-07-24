18e0534c00aba3ba9035bf27ac8e809e6a7dffd7
caiogomesdev <caiogame100@gmail.com>
2022-07-24 17:19:06 -0300

    feat: integration backend

--- mobile/src/hooks/auth.tsx
+++ mobile/src/hooks/auth.tsx
@@ -1,16 +1,19 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import { httpService } from '../services';
 
 interface IAuth {
   isLogged: boolean;
   user: IUser | null;
+  logout: () => void;
+  signIn: (login: ISignIn) => Promise<void>;
 }
 
-interface SignIn {
+export interface ISignIn {
   email: string,
-  passWord: string,
+  password: string,
 }
 
-interface IUser extends SignIn {
+export interface IUser extends ISignIn {
   name: string,
 }
 
@@ -18,14 +21,33 @@ export const AuthContext = createContext<IAuth | null>(null);
 const AuthProvider: React.FC = ({ children }) => {
   const [ user, setUser ] = useState<IUser | null>(null);
 
-  async function handleSignUp({ name, email, passWord }: IUser){
+  useEffect(() => {
+    signInWithToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJmMmExOWIyZC1mMTkxLTRhODctYjAxOS00N2I5MDIyOTljNmUiLCJpYXQiOjE2NTg2MTUzNDksImV4cCI6MTY1ODc4ODE0OX0.WE1ii4sX2CKi20tMxTgi0Hnr9TH1yBnLFqMkPtR_H1Q');
+  }, [])
+
+  async function signInWithToken(token: string): Promise<void> {
+    try {
+      httpService.changeAcessToken(token);
+      const result = await httpService.meRoute();
+      setUser(result.data);
+    } catch {
+      setUser(null);
+    }
   }
 
-  return(
-    <AuthContext.Provider value={{ isLogged: !!user, user }}>
+  async function signIn(body: ISignIn): Promise<void> {
+    const result = await httpService.signinRoute(body);
+    signInWithToken(result);
+  }
+
+  function logout(): void {
+    setUser(null)
+  }
+
+  return (
+    <AuthContext.Provider value={{ isLogged: !!user, user, logout, signIn }}>
       {children}
     </AuthContext.Provider>
-
   )
 }
 
