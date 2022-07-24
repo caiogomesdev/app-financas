a1840277faa04adfece8a3eca4cd38a764084433
caiogomesdev <caiogame100@gmail.com>
2022-07-24 17:16:04 -0300

    fix: change baseurl

--- mobile/src/services/httpClient.ts
+++ mobile/src/services/httpClient.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseURL = process.env.EXPO_BASE_URL
+console.log('process.env.EXPO_BASE_URL', baseURL)
 const httpClient = axios.create({
-  baseURL: process.env.BASE_URL
+  baseURL
 })
 
 export { httpClient };
