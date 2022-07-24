af60c91f7a67ae186cb033ef644c9098a45e86e3
caiogomesdev <caiogame100@gmail.com>
2022-07-24 17:54:23 -0300

    feat: integration view home with getAllPricesRoute

--- mobile/src/views/Home/index.tsx
+++ mobile/src/views/Home/index.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Section } from './styles';
 import Header from '../../components/Header';
 import Tabs from '../../components/Tabs';
 import CardsContainer from '../../components/CardsContainer';
+import { httpService } from '../../services';
 
 const App: React.FC = () => {
+  const [ priceTotal, setPriceTotal] = useState(0)
+  useEffect(() => {
+    init();
+  }, []);
+
+  async function init() {
+    await getTotalPrices();
+  }
+
+  async function getTotalPrices(){
+    const result = await httpService.getAllPricesRoute();
+    setPriceTotal(result);
+  }
+
   return (
     <Container>
-      <Header></Header>
+      <Header priceTotal={ priceTotal }></Header>
       <Section>
         <Tabs />
         <CardsContainer />
