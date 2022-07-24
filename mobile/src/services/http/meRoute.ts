8956064f6dbe672b4787b5b35b0f6f195d4a22b3
caiogomesdev <caiogame100@gmail.com>
2022-07-24 17:17:35 -0300

    feat: create meRoute

---
+++ mobile/src/services/http/meRoute.ts
@@ -0,0 +1,8 @@
+import { AxiosRequestConfig, AxiosResponse } from "axios";
+import { IUser } from "../../hooks/auth";
+import { ROUTES } from "../../utils/constants";
+import { httpClient } from "../httpClient";
+
+export const meRoute = (config: AxiosRequestConfig): Promise<AxiosResponse<IUser, any>> => {
+  return httpClient.get<IUser>(ROUTES.ME_ROUTE, config );
+}
